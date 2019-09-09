ab50c184b9ad6d5341c4ca4620a363d9456b50b1
nightblade9 <nightbladecodes@gmail.com>
2019-09-08 22:00:09 -0400

    Monsters appear in the room listing now

--- Prototype/Game/Models/Room.cs
+++ Prototype/Game/Models/Room.cs
@@ -24,7 +24,50 @@ namespace Prototype.Game.Models
         {
             this.Id = unusedRoomIds[random.Next(unusedRoomIds.Count)];
             unusedRoomIds.Remove(this.Id);
+            this.GenerateMonsters(numMonsters);
+        }
 
+        public void ConnectTo(Room target)
+        {
+            if (!connectedTo.Contains(target))
+            {
+                connectedTo.Add(target);
+            }
+        }
+
+        public string GetContents()
+        {
+            var builder = new StringBuilder();
+            string numberOfMonsters = this.Monsters.Any() ? this.Monsters.Count.ToString() : "no";
+            builder.Append($"You are in the {this.Id} room. ");
+
+            builder.Append($"This room contains {numberOfMonsters} monsters: ");
+            foreach (var monsterGroup in this.Monsters.GroupBy(m => m.Name))
+            {
+                builder.Append($"{monsterGroup.Count()} {monsterGroup.Key}s, ");
+            }
+
+            builder.Append(" This room connects to ");
+            foreach (var room in this.connectedTo)
+            {
+                builder.Append($"The {room.Id} room, ");
+
+                if (this.connectedTo.Count > 1 && room == this.connectedTo[this.connectedTo.Count - 2])
+                {
+                    builder.Append(" and ");
+                }
+            };
+
+            return builder.ToString();
+        }
+
+        internal bool IsConnectedTo(Room target)
+        {
+            return this.connectedTo.Contains(target);
+        }
+
+        private void GenerateMonsters(int numMonsters)
+        {
             var totalMonsterProbability = GlobalConfig.MONSTER_PROBABILITY.Values.Sum();
             var weightSoFar = 0f;
             while (Monsters.Count < numMonsters)
@@ -55,37 +98,6 @@ namespace Prototype.Game.Models
                     }
                 }
             }
-        }
-
-        public void ConnectTo(Room target)
-        {
-            if (!connectedTo.Contains(target))
-            {
-                connectedTo.Add(target);
-            }
-        }
-
-        public string GetContents()
-        {
-            var builder = new StringBuilder();
-            builder.Append($"You are in the {this.Id} room. This room connects to ");
-
-            foreach (var room in this.connectedTo)
-            {
-                builder.Append($"The {room.Id} room, ");
-
-                if (this.connectedTo.Count > 1 && room == this.connectedTo[this.connectedTo.Count - 2])
-                {
-                    builder.Append(" and ");
-                }
-            };
-
-            return builder.ToString();
-        }
-
-        internal bool IsConnectedTo(Room target)
-        {
-            return this.connectedTo.Contains(target);
         }
     }
 }
