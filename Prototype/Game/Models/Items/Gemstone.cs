633bccf43d4d1fa0c529dc32b58dbedf30c77e73
nightblade9 <nightbladecodes@gmail.com>
2019-09-19 13:04:44 -0400

    Gemstone class.

---
+++ Prototype/Game/Models/Items/Gemstone.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Prototype.Game.Models.Items
+{
+    class Gemstone : AbstractItem
+    {
+        private static List<string> UniqueDescriptions = new List<string>() { "shiny", "smooth", "ridged", "oblong", "round", "angular", "heavy", "bright", "cold", "warm", "metallic" };
+        private static int nextId = 0;
+
+        private string id = "";
+
+        static Gemstone()
+        {
+            var random = new Random();
+            Gemstone.UniqueDescriptions = Gemstone.UniqueDescriptions.OrderBy(g => random.Next()).ToList();
+        }
+
+        public Gemstone()
+        {
+            this.id = Gemstone.UniqueDescriptions[Gemstone.nextId];
+            Gemstone.nextId++;
+        }
+
+        public override string Description => $"A {this.id} gemstone";
+    }
+}
