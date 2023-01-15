5eceb85e7e541fc2454b8f94ab2f7abd18a91ac5
nightblade9 <nightbladecodes@gmail.com>
2023-01-15 17:09:23 -0500

    Disable text-to-speech; use .NET Core 7.0 to build.

--- Prototype/TextToSpeech/MicrosoftSpeaker.cs
+++ Prototype/TextToSpeech/MicrosoftSpeaker.cs
@@ -1,7 +1,7 @@
 using System;
 using System.Collections.Generic;
-using System.Speech.AudioFormat;
-using System.Speech.Synthesis;
+// using System.Speech.AudioFormat;
+// using System.Speech.Synthesis;
 using System.Threading;
 
 namespace Prototype.TextToSpeech
@@ -12,8 +12,8 @@ namespace Prototype.TextToSpeech
     /// </summary>
     public class MicrosoftSpeaker : ISpeaker
     {
-        private SpeechSynthesizer synthesizer;
-        private Prompt lastSpoken;
+        // private SpeechSynthesizer synthesizer;
+        // private Prompt lastSpoken;
         private List<string> queueToSpeak = new List<string>();
         private Thread queueThread;
         private bool IsActive = true;
@@ -21,116 +21,116 @@ namespace Prototype.TextToSpeech
         static void PrintAllVoices()
         {
             // Initialize a new instance of the SpeechSynthesizer.  
-            using (SpeechSynthesizer synth = new SpeechSynthesizer())
-            {
+            // using (SpeechSynthesizer synth = new SpeechSynthesizer())
+            // {
 
-                // Output information about all of the installed voices.   
-                Console.WriteLine("Installed voices -");
-                foreach (InstalledVoice voice in synth.GetInstalledVoices())
-                {
-                    VoiceInfo info = voice.VoiceInfo;
-                    string AudioFormats = "";
-                    foreach (SpeechAudioFormatInfo fmt in info.SupportedAudioFormats)
-                    {
-                        AudioFormats += String.Format("{0}\n",
-                            fmt.EncodingFormat.ToString());
-                    }
+            //     // Output information about all of the installed voices.   
+            //     Console.WriteLine("Installed voices -");
+            //     foreach (InstalledVoice voice in synth.GetInstalledVoices())
+            //     {
+            //         VoiceInfo info = voice.VoiceInfo;
+            //         string AudioFormats = "";
+            //         foreach (SpeechAudioFormatInfo fmt in info.SupportedAudioFormats)
+            //         {
+            //             AudioFormats += String.Format("{0}\n",
+            //                 fmt.EncodingFormat.ToString());
+            //         }
 
-                    Console.WriteLine(" Name:          " + info.Name);
-                    Console.WriteLine(" Culture:       " + info.Culture);
-                    Console.WriteLine(" Age:           " + info.Age);
-                    Console.WriteLine(" Gender:        " + info.Gender);
-                    Console.WriteLine(" Description:   " + info.Description);
-                    Console.WriteLine(" ID:            " + info.Id);
-                    Console.WriteLine(" Enabled:       " + voice.Enabled);
-                    if (info.SupportedAudioFormats.Count != 0)
-                    {
-                        Console.WriteLine(" Audio formats: " + AudioFormats);
-                    }
-                    else
-                    {
-                        Console.WriteLine(" No supported audio formats found");
-                    }
+            //         Console.WriteLine(" Name:          " + info.Name);
+            //         Console.WriteLine(" Culture:       " + info.Culture);
+            //         Console.WriteLine(" Age:           " + info.Age);
+            //         Console.WriteLine(" Gender:        " + info.Gender);
+            //         Console.WriteLine(" Description:   " + info.Description);
+            //         Console.WriteLine(" ID:            " + info.Id);
+            //         Console.WriteLine(" Enabled:       " + voice.Enabled);
+            //         if (info.SupportedAudioFormats.Count != 0)
+            //         {
+            //             Console.WriteLine(" Audio formats: " + AudioFormats);
+            //         }
+            //         else
+            //         {
+            //             Console.WriteLine(" No supported audio formats found");
+            //         }
 
-                    string AdditionalInfo = "";
-                    foreach (string key in info.AdditionalInfo.Keys)
-                    {
-                        AdditionalInfo += String.Format("  {0}: {1}\n", key, info.AdditionalInfo[key]);
-                    }
+            //         string AdditionalInfo = "";
+            //         foreach (string key in info.AdditionalInfo.Keys)
+            //         {
+            //             AdditionalInfo += String.Format("  {0}: {1}\n", key, info.AdditionalInfo[key]);
+            //         }
 
-                    Console.WriteLine(" Additional Info - " + AdditionalInfo);
-                    Console.WriteLine();
-                }
-            }
+            //         Console.WriteLine(" Additional Info - " + AdditionalInfo);
+            //         Console.WriteLine();
+            //     }
+            // }
         }
 
         public MicrosoftSpeaker()
         {
-            // Initialize a new instance of the SpeechSynthesizer.  
-            this.synthesizer = new SpeechSynthesizer();
-            // Configure the audio output. 
-            this.synthesizer.SetOutputToDefaultAudioDevice();
-            this.synthesizer.SelectVoiceByHints(VoiceGender.Female);
+            // // Initialize a new instance of the SpeechSynthesizer.  
+            // this.synthesizer = new SpeechSynthesizer();
+            // // Configure the audio output. 
+            // this.synthesizer.SetOutputToDefaultAudioDevice();
+            // this.synthesizer.SelectVoiceByHints(VoiceGender.Female);
 
-            this.queueThread = new Thread(() =>
-             {
-                 while (this.IsActive)
-                 {
-                     this.OnUpdate();
-                 }
-             });
+            // this.queueThread = new Thread(() =>
+            //  {
+            //      while (this.IsActive)
+            //      {
+            //          this.OnUpdate();
+            //      }
+            //  });
 
-            this.queueThread.Start();
+            // this.queueThread.Start();
         }
 
         public void Speak(string text)
         {
-            if (this.lastSpoken != null && !this.lastSpoken.IsCompleted)
-            {
-                queueToSpeak.Add(text);
-            }
-            else
-            {
-                this.lastSpoken = synthesizer.SpeakAsync(text);
-            }
+            // if (this.lastSpoken != null && !this.lastSpoken.IsCompleted)
+            // {
+            //     queueToSpeak.Add(text);
+            // }
+            // else
+            // {
+            //     this.lastSpoken = synthesizer.SpeakAsync(text);
+            // }
         }
 
         public void FinishSpeaking()
         {
-            while (this.queueToSpeak.Count > 0 || (this.lastSpoken != null && !this.lastSpoken.IsCompleted))
-            {
-                System.Threading.Thread.Sleep(100);
-            }
+            // while (this.queueToSpeak.Count > 0 || (this.lastSpoken != null && !this.lastSpoken.IsCompleted))
+            // {
+            //     System.Threading.Thread.Sleep(100);
+            // }
 
-            this.IsActive = false;
+            // this.IsActive = false;
 
-            queueThread.Join();
+            // queueThread.Join();
         }
 
         public void StopAndClearQueue()
         {
-            synthesizer.SpeakAsyncCancelAll();
-            this.lastSpoken = null;
-            this.queueToSpeak.Clear();
+            // synthesizer.SpeakAsyncCancelAll();
+            // this.lastSpoken = null;
+            // this.queueToSpeak.Clear();
         }
 
         private void OnUpdate()
         {
             // Concurrent access to this.lastSpoken; sometimes this.lastSpoken is not null but immediately this.lastSpoken.IsCompleted
             // throws a null exception. Wierd.
-            try
-            {
-                if (this.lastSpoken != null && this.lastSpoken.IsCompleted && this.queueToSpeak.Count > 0)
-                {
-                    var text = this.queueToSpeak[0];
-                    this.queueToSpeak.RemoveAt(0);
-                    this.lastSpoken = synthesizer.SpeakAsync(text);
-                }
-            }
-            catch
-            {
+            // try
+            // {
+            //     if (this.lastSpoken != null && this.lastSpoken.IsCompleted && this.queueToSpeak.Count > 0)
+            //     {
+            //         var text = this.queueToSpeak[0];
+            //         this.queueToSpeak.RemoveAt(0);
+            //         this.lastSpoken = synthesizer.SpeakAsync(text);
+            //     }
+            // }
+            // catch
+            // {
 
-            }
+            // }
         }
     }
 }
